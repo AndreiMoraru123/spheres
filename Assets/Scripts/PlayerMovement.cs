c18fed3c4598b1997f5e31fbcd67da6ef47c621a
AndreiMoraru123 <81184255+AndreiMoraru123@users.noreply.github.com>
2023-09-16 21:23:20 +0300

    updates player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,5 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-using Vector3 = System.Numerics.Vector3;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -15,7 +14,7 @@ public class PlayerMovement : MonoBehaviour
     private void OnMove(InputAction.CallbackContext context)
     {
         // Get the movement vector and apply it to the RollingMovement component
-        Vector2 movement = context.ReadValue<Vector2>(); // callback for hitting arrow keys
-        _mRollingMovement.mMovementDirection = new UnityEngine.Vector3(movement.x, 0f, movement.y);
+        var  movement = context.ReadValue<Vector2>(); // callback for hitting arrow keys
+        _mRollingMovement.mMovementDirection = new Vector3(movement.x, 0f, movement.y);
     }
 }
